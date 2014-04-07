23a4e4f458dfccc462c34e5d4f30d8de391ee05e
Carlos Rodrigues <carlos.rodrigues@pinnacol.com>
2014-04-07 12:51:49 -0600

    Now checks whether the given files have any of the search terms for the ICD9 changes

--- find_search_term_in_forms_xml.sh
+++ find_search_term_in_forms_xml.sh
@@ -1,9 +1,20 @@
 #! /bin/bash
 
+res=results
+mkdir -p results
+
 shopt -s nullglob  # so that if there are no xml files, that f does not get set to '*.xml' below
 
 # iterate over the xml files in the current directory
 for f in *.xml; do
-	echo $f
+	# find the search terms for icd9
+	found="$res/found_in_$f"
+	grep -i -e '[^u]pda' -e 'icd' "$f" > $found
+	
+	# delete the file if it is empty
+	if [ -e $found -a ! -s $found ]; then
+		echo "$f does not contain the search terms"
+		rm -f $found
+	fi
 done
 
