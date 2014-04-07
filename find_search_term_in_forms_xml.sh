8e8bbcf8060d13511f9f1080a1c7fd49e4452a7d
Carlos Rodrigues <carlos.rodrigues@pinnacol.com>
2014-04-07 12:10:18 -0600

    Now handles the case when there are no xml files in the current directory

--- find_search_term_in_forms_xml.sh
+++ find_search_term_in_forms_xml.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+shopt -s nullglob  # so that if there are no xml files, that f does not get set to '*.xml' below
+
 # iterate over the xml files in the current directory
 for f in *.xml; do
 	echo $f
