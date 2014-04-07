53eeae0a5bfb79885f56808ca7befc387cda05e7
Carlos Rodrigues <carlos.rodrigues@pinnacol.com>
2014-04-07 10:57:47 -0600

    Script now deletes result files if they're empty

--- get_forms_and_make_xml.sh
+++ get_forms_and_make_xml.sh
@@ -15,9 +15,15 @@ for i in "${forms[@]}"; do
 	fi
 	
 	# find other forms called from this file
+	called_forms="forms_called_from_$i"
 	grep -in -e wcis_call_form -e wcis_security\.callform $xml \
 		| sed "s/\([0-9]\+\).\+wcis_security\.callform('\?\([^',)]\+\)'\?.\+/\1: \2/i" \
 		| sed "s/\([0-9]\+\).\+wcis_call_form('\?\([^',)]\+\)'\?).\+/\1: \2/i" 
-		> forms_called_from_$i 
+		> $called_forms
+	# delete the file if it is empty
+	if [ -e $called_forms -a ! -s $called_forms ]; then
+		echo "$i does not call any forms"
+		rm -f $called_forms
+	fi
 done
 
